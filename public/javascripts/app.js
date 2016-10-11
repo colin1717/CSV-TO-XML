cb3a3e80b84892f59b3e9b940408034d54cd8b14
colin1717 <colin1717@mac.com>
2016-10-11 16:40:58 -0500

    ui-router is working

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,1 +1,18 @@
-angular.module('CsvToXml', []);
+angular
+    .module('CsvToXml', ['ui.router'])
+    .config(MainRouter);
+
+  MainRouter.$inject = ['$stateProvider', '$urlRouterProvider'];
+  function MainRouter($stateProvider, $urlRouterProvider){
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: '../templates/home.html'
+      })
+      .state('output', {
+        url: '/output',
+        templateUrl: '../templates/output.html'
+      });
+
+      $urlRouterProvider.otherwise('/');
+  }
