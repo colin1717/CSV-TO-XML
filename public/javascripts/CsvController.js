863f63d7b66b3f86fbfdbb66cc40248171adb6b0
colin1717 <colin1717@mac.com>
2016-10-17 14:50:14 -0500

    conversion working

--- public/javascripts/CsvController.js
+++ public/javascripts/CsvController.js
@@ -14,12 +14,37 @@ function CsvController () {
     self.jsonOutput = Papa.parse(self.csvInput);
   };
 
-  this.wtf = function(){
-    console.log('idontknow man');
+  this.jsonToXml = function(){
+    self.xmlOutput = '<?xml version="1.0" encoding="utf-8"?><Feed xmlns="http://www.bazaarvoice.com/xs/PRR/StandardClientFeed/5.6" name="ExampleClient" extractDate="2012-02-12T05:17:33.945-06:00">';
+
+    for (var i = 1; i < (self.jsonOutput.data.length); i++){
+      // self.xmlOutput += '<Product id="' + {self.jsonOutput.data[i][4]} + '">';
+      self.xmlOutput += '<Product id="' + self.jsonOutput.data[i][4] +
+      '"><ExternalId>'+ self.jsonOutput.data[i][4] +'</ExternalID>' +
+      '<Reviews>' +
+      '<Review>' +
+      '<ModerationStatus>' + self.jsonOutput.data[i][8] + '</ModerationStatus>'
+      +'<UserProfileReference id="' + self.jsonOutput.data[i][3] +'">'
+      +'<ExternalId>' + self.jsonOutput.data[i][3] + '</ExternalId>'
+      +'</UserProfileReference>'
+      +'<Title>' + self.jsonOutput.data[i][1] + '</Title>'
+      +'<ReviewText>' + self.jsonOutput.data[i][2] + '</ReviewText>'
+      +'<Rating>' + self.jsonOutput.data[i][7] + '</Rating>'
+      +'<IpAddress>' + self.jsonOutput.data[i][6] + '</IpAddress>'
+      +'<ReviewerLocation>' + self.jsonOutput.data[i][5] + '</ReviewerLocation>'
+      +'<SubmissionTime>' + self.jsonOutput.data[i][0] + '</SubmissionTime>'
+
+      +'</Review>'
+      +'</Reviews>'
+      + "</Product>";
+    }
+
+    self.xmlOutput += '</Feed>';
+
+    console.log(self.jsonOutput.data);
   }
 
-  this.jsonToXml = function(){
-    self.xmlOutput = '<?xml version="1.0" encoding="utf-8"?>' + self.jsonOutput.data[0];
+  this.parseJson = function(){
 
   }
 
